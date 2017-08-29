e06106560b978074af8105bc71f3df6a9ab32643
Kirill Lykho <kirill.lykho@nixsolutions.com>
2017-08-29 12:41:53 +0300

    add es5 ternary templates

--- mexna.js
+++ mexna.js
@@ -9,17 +9,19 @@ module.exports = mexna;
 var DELIMETER = '||';
 var REGEX = /\$\{(.+?)\}/g;
 var EXPOSE_OUT_REGEX = /"?<!(.+?)!>"?/g;
+var TERNARY_REGEX = /(.+)\s*\?\s*(.+)\s*:\s*(.+)/g;
 
 /**
  * @public
- * @param {string} str
+ * @param {string} defaultStr
  * @param {object} options
  * @return {string}
  */
-function mexna(str, options) {
+function mexna(defaultStr, options) {
     options = _.extend({
         regex: REGEX,
         delimeter: DELIMETER,
+        ternaryRegex: TERNARY_REGEX,
         keys: {},
         i18n: {},
         translate: false
@@ -31,63 +33,81 @@ function mexna(str, options) {
 
     var isExposeOut = false;
 
-    var replaceValue = str.replace(options.regex, function (match, expression) {
-        var key = expression;
-        var defaultValue = null;
-        var value = null;
-        var keys = options.keys;
-        var keyValue;
-
-        if (includes(expression, options.delimeter)) {
-            var parts = expression.split(options.delimeter);
-            if (parts.length !== 2) {
-                throw new Error('Syntax Error: ' + expression + ' has no valid default value');
+    var strReplace = function(str) {
+        return str.replace(options.regex, function (match, expression) {
+            options.ternaryRegex.lastIndex = 0;
+            var key = expression;
+            var defaultValue = null;
+            var value = null;
+            var keys = options.keys;
+            var keyValue;
+            var isTernary = options.ternaryRegex.test(expression);
+            if (isTernary) {
+                var prop = key.split('?')[0].trim();
+                var splitted = key.split('?')[1].split(':');
+                var expandable = splitted[0].trim();
+                var nonExpandable = splitted[1].trim();
+                expandable = expandable.replace('#(', '${').replace(')#', '}');
+                if (!options.keys[prop]) {
+                    return nonExpandable;
+                } else {
+                    return strReplace(expandable);
+                }
             }
 
-            var defaultValueExpression = _.trim(parts.pop().trim());
-            key = _.trim(parts.pop());
+            if (includes(expression, options.delimeter)) {
+                var parts = expression.split(options.delimeter);
+                if (parts.length !== 2) {
+                    throw new Error('Syntax Error: ' + expression + ' has no valid default value');
+                }
 
-            if (options.keys[key] === undefined) {
-                try {
-                    defaultValue = JSON.parse(defaultValueExpression);
-                    if (options.translate) {
-                        defaultValue = options.i18n[defaultValue];
-                    }
-                } catch (e) {
-                    if (!_isString(defaultValueExpression)) {
-                        defaultValue = defaultValueExpression;
+                var defaultValueExpression = _.trim(parts.pop().trim());
+                key = _.trim(parts.pop());
+
+                if (options.keys[key] === undefined) {
+                    try {
+                        defaultValue = JSON.parse(defaultValueExpression);
+                        if (options.translate) {
+                            defaultValue = options.i18n[defaultValue];
+                        }
+                    } catch (e) {
+                        if (!_isString(defaultValueExpression)) {
+                            defaultValue = defaultValueExpression;
+                        }
                     }
                 }
             }
-        }
 
-        if (options.strict && !options.keys.hasOwnProperty(key) && !defaultValue) {
-            throw new Error('Range Error: `' + key + '` is not defined. '
-              + 'Consider adding it to the `keys` option or turning off the strict mode.');
-        }
-
-        keyValue = keys[key];
-        value = keyValue === '' ? keyValue : (keyValue || defaultValue || '');
-
-        if (typeof value === 'function') {
-            value = value(key);
-        }
-
-        if (value && options.translate && typeof value === 'string') {
-            value = options.i18n[value] || value;
-        }
-
-        if (options.exposeOut) {
-            if (typeof value !== 'string') {
-                value = '<!' + JSON.stringify(value) + '!>';
+            if (options.strict && !options.keys.hasOwnProperty(key) && !defaultValue) {
+                throw new Error('Range Error: `' + key + '` is not defined. '
+                    + 'Consider adding it to the `keys` option or turning off the strict mode.');
             }
-            isExposeOut = true;
-        }
 
-        return typeof value !== 'string' ?
-            JSON.stringify(value) :
-            value;
-    });
+            keyValue = keys[key];
+            value = keyValue === '' ? keyValue : (keyValue || defaultValue || '');
+
+            if (typeof value === 'function') {
+                value = value(key);
+            }
+
+            if (value && options.translate && typeof value === 'string') {
+                value = options.i18n[value] || value;
+            }
+
+            if (options.exposeOut) {
+                if (typeof value !== 'string') {
+                    value = '<!' + JSON.stringify(value) + '!>';
+                }
+                isExposeOut = true;
+            }
+
+            return typeof value !== 'string' ?
+                JSON.stringify(value) :
+                value;
+        });
+    };
+
+    var replaceValue = strReplace(defaultStr);
 
     return isExposeOut ?
         replaceValue.replace(EXPOSE_OUT_REGEX, _exposeOutReplacer) :
